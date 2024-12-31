14dfe25f51bcca293a81a85aa7f0b0257235ffd2
Neel Bhattacharya <neelbhatta22@gmail.com>
2024-12-31 23:14:08 +0530

    frontend in progress :memo:

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,19 +1,74 @@
 import React from 'react'
 import { SidebarProvider } from './ui/sidebar'
+import { Input } from './ui/input'
 import Mainfriend from './Mainfriend'
+import { Button } from './ui/button'
+
+
+// button
+import { Send, SendHorizontal } from 'lucide-react';
 
 const Dashboard = () => {
     return (
         <div className='flex h-screen bg-gray-950'>
-            <div className='h-full w-1/6'>                
+
+            {/* left component */}
+            <div className='h-full w-1/6'>
                 {/* left component */}
                 <SidebarProvider className="bg-[#272932] border-r-2 border-gray-700 shadow-gray-400 shadow-md">
                     <Mainfriend className="h-full w-full" />
                 </SidebarProvider>
             </div>
-            <div className='h-screen w-5/6'>
-                {/* right component */}
+
+
+            {/* right component */}
+            <div className="h-screen w-5/6 flex">
+                {/* Right component */}
+                <div className="w-full h-full flex flex-col">
+                    {/* Top part (Header with name and profile picture) */}
+                    <div className="w-full bg-gray-800  flex items-center p-4">
+                        <img
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNbkECXtEG_6-RV7CSNgNoYUGZE-JCliYm9g&s"
+                            alt="Profile"
+                            className="w-10 h-10 mr-4 border-2 border-gray-50 rounded-full hover:cursor-pointer"
+                        />
+                        <span className="text-white font-medium text-lg hover:cursor-pointer">Person's Name</span>
+                    </div>
+                    {/* line */}
+                    <hr className="border-t-2 border-gray-400 my-0" />
+
+                    {/* Middle part (Chat display) */}
+                    <div className="w-full flex-1 bg-gray-900 overflow-y-auto">
+                        {/* Chat content */}
+                        <div className="p-4 space-y-4">
+                            <div className="text-white bg-blue-500 rounded-lg p-3 w-fit self-end">
+                                Hello!
+                            </div>
+                            <div className="text-white bg-gray-600 rounded-lg p-3 w-fit self-start">
+                                Hi there!
+                            </div>
+                            {/* Add more messages */}
+                        </div>
+                    </div>
+
+
+                    {/* line */}
+                    <hr className="border-t-2 border-gray-400 my-0" />
+
+                    {/* Bottom part (Input area) */}
+                    <div className="w-full bg-gray-800 p-4 flex items-center">
+                        <Input
+                            type="text"
+                            placeholder="Type your message..."
+                            className="flex-1 rounded-xl border-gray-500 p-2 bg-gray-700 border-none  placeholder:text-gray-400 text-gray-300"
+                        />
+                        <Button className="ml-4 bg-blue-700 text-white p-4 rounded-xl hover:bg-blue-900">
+                            <SendHorizontal/>
+                        </Button>
+                    </div>
+                </div>
             </div>
+
         </div>
     )
 }
--- frontend/src/components/Mainfriend.jsx
+++ frontend/src/components/Mainfriend.jsx
@@ -19,14 +19,14 @@ const Mainfriend = () => {
         <div className='h-screen w-full bg-gray-900 flex flex-col'>
             {/*search username*/}
             <SidebarHeader className="bg-gray-800 p-4">
-                <Input type="text" placeholder="Search Friend" className="text-gray-500 border-none bg-gray-900 rounded-xl">
+                <Input type="text" placeholder="Search Friend" className="placeholder:text-gray-500 text-gray-300 border-none bg-gray-900 rounded-xl">
                 </Input>
                 <Button className="bg-blue-700 hover:bg-blue-900 rounded-xl text-white">
                     Search
                 </Button>
             </SidebarHeader>
             {/* line */}
-            <hr className="border-t-2 border-gray-600 my-0" />
+            <hr className="border-t-2 border-gray-400 my-0" />
 
             {/* friend list */}
             <SidebarContent className="flex-1 overflow-y-auto scrollbar scrollbar-thin scrollbar-thumb-blue-500 scrollbar-track-gray-700">
@@ -52,15 +52,26 @@ const Mainfriend = () => {
             </SidebarContent>
 
             {/* footer */}
-            <SidebarFooter className="bg-gray-800 p-4">
-                <Input type="text" placeholder="Search Username" className="text-gray-500 border-none rounded-xl bg-gray-900">
+            <SidebarFooter className="bg-gray-800 p-2 border-t-2 border-gray-400">
+                {/* <Input type="text" placeholder="Search Username" className="text-gray-500 border-none rounded-xl bg-gray-900">
                 </Input>
                 <Button className="bg-blue-700 hover:bg-blue-900 rounded-xl text-white">
                     Send request
-                </Button>
+                </Button> */}
+
+                <div className="flex items-center">
+                    <img
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvCvhJhHOUxxRoXZGNwYomsL9ms-atp-p9eA&s"                        
+                        className="w-10 h-10 rounded-full mr-4"
+                    />
+                    <span className="text-gray-300 hover:cursor-pointer">Main User</span>
+                </div>
+
             </SidebarFooter>
         </div>
     )
 }
 
 export default Mainfriend
+
+
